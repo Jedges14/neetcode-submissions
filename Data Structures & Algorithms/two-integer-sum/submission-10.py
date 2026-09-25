6b93fa8784b32bc9e610df5b2d56a54196af93d9
Jed <140191721+Jedges14@users.noreply.github.com>
2026-09-25 11:22:48 -0400

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        di = {}
+
+        for i,n in enumerate(nums):
+            if target - n in di:
+                return[di[target - n], i]
+
+            di[n] = i
+
+        return []        
